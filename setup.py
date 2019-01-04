b12dd744d9f4f4840d1fb0f5e2a3529db4a2605f
BuildTools <nickolay02@inbox.ru>
2019-01-04 12:13:16 +0200

    More bug fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='dumbcpm',
-      version='0.2.1',
+      version='0.2.2',
       description='Possibly very dumb package manager for C/++, written in Python.',
       author='Nickolay Ilyushin',
       author_email='nickolay02@inbox.ru',
